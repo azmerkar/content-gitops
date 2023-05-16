27fa9f8d122c9c55a8ea01f26c45de45faf5fb1d
azmerkar <68376823+azmerkar@users.noreply.github.com>
2023-05-16 09:51:46 +0200

    Update app.py
    tag 1.2
    name ist geändert

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Servus AZRA MERYEM"
+    return "Servus AZRA MERYEM KARADAYI"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
